0969a4f96a8b2a314bb83616f377574cb441b0fe
Rogelio García <106293025+MrChilaquiles@users.noreply.github.com>
2024-02-01 11:44:23 -0600

    Add pointers file

---
+++ pointers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nombre;
+    cout<<"Hola mundo, dime tu nombre: ";
+    cin>>nombre;
+    cout<<"\nComo estas " + nombre + "E?";
+    return 8;
+}
