b7790a262441ed2ee4ed920211e45b66b4a17040
Rogelio García <106293025+MrChilaquiles@users.noreply.github.com>
2024-02-03 19:35:49 -0600

    Finish Pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <math.h>
 
-int main(){
-    string nombre;
-    cout<<"Hola mundo, dime tu nombre: ";
-    cin>>nombre;
-    cout<<"\nComo estas " + nombre + "E?";
-    return 8;
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int diff = abs(*a - *b);
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
 }
