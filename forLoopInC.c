d3412ddfc1385bb273782d74768a522997c5e813
MrChilaquiles <RGGA2000@gmail.com>
2024-08-13 20:19:50 -0600

    Solving conditionalStatementsInC

---
+++ forLoopInC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    for (int i = 0; i < ; i++) {
+        /* code */
+    }
+    
+    return 0;
+}
