3e8d44c65284de4a15280ece828e5e0aae806e02
MrChilaquiles <RGGA2000@gmail.com>
2024-08-14 20:36:58 -0600

    Solving forLoopInC

--- forLoopInC.c
+++ forLoopInC.c
@@ -3,13 +3,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
-{
+int main() {
     int a, b;
     scanf("%d\n%d", &a, &b);
-    for (int i = 0; i < ; i++) {
-        /* code */
+    char* numbers[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    for (int i = a; i <= b; i++) {
+        if (i <= 9) {
+            printf("%s\n",numbers[i-1]);
+        } else if (i % 2 == 0) {
+            printf("even\n");
+        } else {
+            printf("odd\n");
+        }
     }
-    
     return 0;
 }
