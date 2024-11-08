2ff5d51da8d18190e9dac4c2c42c78d4c565d418
MrChilaquiles <RGGA2000@gmail.com>
2024-11-07 18:29:35 -0600

    Solving A791_BearAndBigBrother

---
+++ A791_BearAndBigBrother.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int limak, bob, count = 0;
+    cin >> limak >> bob;
+    
+    while (limak <= bob) {
+        limak *= 3;
+        bob *= 2;
+        count++;
+    }
+    
+    cout << count;
+}
