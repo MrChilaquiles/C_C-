c81cd764cb5e64cb40abc37ac22b9317f7217b11
MrChilaquiles <RGGA2000@gmail.com>
2024-11-12 20:00:26 -0600

    Solving A110_NearlyLuckyNumber

--- A110_NearlyLuckyNumber.cpp
+++ A110_NearlyLuckyNumber.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
 using namespace std;
 
-bool lucky(string num) {
-    bool lucky;
+int main() {
+    int len = 0;
+    string num;
+    cin >> num;
+
     for (int i = 0; i < num.length(); i++) {
         if (num[i] == '4' || num[i] == '7') {
-            lucky = true;
-        } else {
-            lucky = false;
-            break;
+            len++;
         }
     }
 
-    return lucky;
-}
-int main() {
-    string num, lenght;
-    cin >> num;
-    lenght = to_string(num.length());
-
-    if (lucky(lenght) == true) {
-        if (lucky(num) == true) {
-            cout << "YES";
-        } else
-        {
-            cout << "NO";
-        }
-        
+    if (len == 4 || len == 7) {
+        cout << "YES";
     } else {
         cout << "NO";
     }
-    
-    
 }
