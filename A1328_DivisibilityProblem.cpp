ff4fc0991eedf2fb489d6781e13ece8a5e0a731c
AuxSistemasMarblock <auxsistemas@marblock.com>
2025-06-23 17:44:21 -0600

    Improves output formatting for divisibility check

--- A1328_DivisibilityProblem.cpp
+++ A1328_DivisibilityProblem.cpp
@@ -8,11 +8,11 @@ int main() {
         int a, b;
         cin >> a >> b;
         if ((a % b) == 0) {
-            cout << 0;
+            cout << 0 << endl;
         } else {
-            cout << (b - (a % b));
+            cout << (b - (a % b)) << endl;
         }
     }
-    
+
     return 0;
 }
