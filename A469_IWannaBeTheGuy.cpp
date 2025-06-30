7e57dd6b44057397ce4e8e82875d0aeb969e68ce
AuxSistemasMarblock <auxsistemas@marblock.com>
2025-06-30 15:44:03 -0600

    Solving A469

---
+++ A469_IWannaBeTheGuy.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n, littleX, littleY;
+    set<int> levels;
+    cin >> n;
+
+    cin >> littleX;
+    for (int i = 0; i < littleX; i++) {
+        int level;
+        cin >> level;
+        levels.insert(level);
+    }
+
+    cin >> littleY;
+    for (int i = 0; i < littleY; i++) {
+        int level;
+        cin >> level;
+        levels.insert(level);
+    }
+    
+    if (levels.size() == n) {
+        cout << "I become the guy.";
+    } else {
+        cout << "Oh, my keyboard!";
+    }
+    
+    return 0;
+}
