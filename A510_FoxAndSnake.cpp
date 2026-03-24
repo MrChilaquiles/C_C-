3993ba9cb5853ebabc8ca0b0495c175cfb61d34d
MrChilaquiles <RGGA2000@gmail.com>
2026-03-24 17:43:11 -0600

    Refactor A510

--- A510_FoxAndSnake.cpp
+++ A510_FoxAndSnake.cpp
@@ -3,31 +3,25 @@
 using namespace std;
 
 int main() {
-    int n, m, hole;
+    int n, m;
     bool right = true;
 
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
-        if ((i % 2) != 0) {
-            hole = right ? m-1 : 0;
+        for (int j = 0; j < m; j++) {
+            if (i % 2 == 0) {
+                cout << "#";
+            } else {
+                if ((right && j == m - 1) || (!right && j == 0)) {
+                    cout << "#";
+                } else {
+                    cout << ".";
+                }
+            }
         }
         
-        for (int j = 0; j < m; j++) {
-            if ((i % 2) != 0) {
-                if (hole == j) {
-                    cout << "#";
-                    continue;
-                }
-                
-                cout << ".";
-                continue;
-            }
-            
-            cout << "#";
-        }
-
-        if ((i % 2) != 0) {
+        if (i % 2 != 0) {
             right = !right;
         }
         
